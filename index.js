528710db8b10db9400ad7480613320709f84c117
Kel Ndu <kelechi.ndumay@gmail.com>
2021-08-19 20:48:20 -0700

    Staging new files

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('resume');
+})
+var port = process.env.PORT || 3000
+app.listen(port, function(){
+    console.log('Server is runing');
+ })
