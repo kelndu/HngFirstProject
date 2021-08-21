46de7a8ddc31f595f4d10f0f78ea0606f743424f
Kel Ndu <kelechi.ndumay@gmail.com>
2021-08-21 08:12:50 -0700

    Added post request response

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     res.render('resume');
 })
+
+app.post('/contact', (req, res)=>{
+    res.send("Form Submitted Thanks");
+})
 var port = process.env.PORT || 3000
 app.listen(port, function(){
     console.log('Server is runing');
